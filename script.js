88a4cde2596de75c03bec330552c7bb2aff36564
NinhHuynh201 <khuongninh239@gmail.com>
2024-10-21 22:28:37 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,189 @@
+const tests = {
+  test1: { // I Phi-líp đoạn 2 câu 1 đến câu 18.
+      questions: [
+        "Câu 1 bài I Phi-líp đoạn 2",
+        "Câu 2 bài I Phi-líp đoạn 2", 
+        "Câu 3 bài I Phi-líp đoạn 2",
+        "Câu 4 bài I Phi-líp đoạn 2",
+        "Câu 5 bài I Phi-líp đoạn 2",
+        "Câu 6 bài I Phi-líp đoạn 2",
+        "Câu 7 bài I Phi-líp đoạn 2",
+        "Câu 8 bài I Phi-líp đoạn 2",
+        "Câu 9 bài I Phi-líp đoạn 2",
+        "Câu 10 bài I Phi-líp đoạn 2",
+        "Câu 11 bài I Phi-líp đoạn 2",
+        "Câu 12 bài I Phi-líp đoạn 2",
+        "Câu 13 bài I Phi-líp đoạn 2",
+        "Câu 14 bài I Phi-líp đoạn 2",
+        "Câu 15 bài I Phi-líp đoạn 2",
+        "Câu 16 bài I Phi-líp đoạn 2",
+        "Câu 17 bài I Phi-líp đoạn 2",
+        "Câu 18 bài I Phi-líp đoạn 2",
+      ],
+      correctAnswers: [
+        // Câu 1
+        "Vậy nếu trong Đấng Christ có điều yên ủi nào, nếu vì lòng yêu thương có điều cứu giúp nào, nếu có sự thông công nơi Thánh Linh, nếu có lòng yêu mến và lòng thương xót,",
+        // Câu 2 
+        "thì anh em hãy hiệp ý với nhau,đồng tình yêu thương, đồng tâm, đồng tư tưởng mà làm cho tôi vui mừng trọn vẹn.", 
+        // Câu 3
+        "Chớ làm sự chi vì lòng tranh cạnh hoặc vì hư vinh, nhưng hãy khiêm nhường, coi người khác như tôn trọng hơn mình.",
+        // Câu 4
+        "Mỗi một người trong anh em chớ chăm về lợi riêng mình, nhưng phải chăm về lợi kẻ khác nữa.",
+        // Câu 5
+        "Hãy có đồng một tâm tình như Đấng Christ đã có,",
+        // Câu 6
+        "Ngài vốn có hình Đức Chúa Trời, song chẳng coi sự bình đẳng mình với Đức Chúa Trời là sự nên nắm giữ;",
+        // Câu 7
+        "chính Ngài đã tự bỏ mình đi, lấy hình tôi tớ và trở nên giống như loài người;",
+        // Câu 8
+        "Ngài đã hiện ra như một người, tự hạ mình xuống, vâng phục cho đến chết, thậm chí chết trên cây thập tự.",
+        // Câu 9
+        "Cũng vì đó nên Đức Chúa Trời đã đem Ngài lên rất cao, và ban cho Ngài danh trên hết mọi danh,",
+        // Câu 10
+        "hầu cho nghe đến danh Đức Chúa Giê-xu, mọi đầu gối trên trời, dưới đất, bên dưới đất, thảy đều quì xuống,",
+        // Câu 11
+        "và mọi lưỡi thảy đều xưng Jêsus Christ là Chúa, mà tôn vinh Đức Chúa Trời, là Đức Chúa Cha.",
+        // Câu 12
+        "Ấy vậy, hỡi những kẻ rất yêu dấu của tôi, như anh em đã vâng lời luôn luôn, chẳng những khi tôi có mặt mà thôi,lại bây giờ là lúc tôi vắng mặt, hãy càng hơn nữa, mà lấy lòng sợ sệt run rẩy làm nên sự cứu chuộc mình.a",
+        // Câu 13
+        "Vì ấy chính Đức Chúa Trời cảm động lòng anh em vừa muốn vừa làm theo ý tốt Ngài.",
+        // Câu 14
+        "Phàm làm việc gì chớ nên lằm bằm và lưỡng lự,",
+        // Câu 15
+        "hầu cho anh em ở giữa dòng dõi hung ác ngang nghịch, được nên con cái của Đức Chúa Trời, không vít, không tì,không chỗ trách được, lại giữa dòng dõi đó, giữ lấy đạo sự sống, chiếu sáng như đuốc trong thế gian;",
+        // Câu 16
+        "cho đến nỗi tới ngày của Đấng Christ, tôi có thể khoe mình rằng chẳng đã chạy vô ích và khó nhọc luống công.",
+        // Câu 17
+        "Ví dầu huyết tôi phải dùng để tưới trên của tế lễ và của dâng đức tin anh em, tôi cũng vui lòng, lại đồng vui với anh em hết thảy.",
+        // Câu 18
+        "Anh em cũng vậy, hãy vui lòng về điều đó, và hãy cùng vui với tôi."
+      ]
+  },
+  test2: { // Demo
+      questions: [
+        "Câu 1 bài I Phi-líp đoạn 2",
+        "Câu 2 bài I Phi-líp đoạn 2", 
+        "Câu 3 bài I Phi-líp đoạn 2",
+        "Câu 4 bài I Phi-líp đoạn 2",
+        "Câu 5 bài I Phi-líp đoạn 2",
+        "Câu 6 bài I Phi-líp đoạn 2",
+        "Câu 7 bài I Phi-líp đoạn 2",
+        "Câu 8 bài I Phi-líp đoạn 2",
+        "Câu 9 bài I Phi-líp đoạn 2",
+        "Câu 10 bài I Phi-líp đoạn 2",
+        "Câu 11 bài I Phi-líp đoạn 2",
+        "Câu 12 bài I Phi-líp đoạn 2",
+        "Câu 13 bài I Phi-líp đoạn 2",
+        "Câu 14 bài I Phi-líp đoạn 2",
+        "Câu 15 bài I Phi-líp đoạn 2",
+        "Câu 16 bài I Phi-líp đoạn 2",
+        "Câu 17 bài I Phi-líp đoạn 2",
+        "Câu 18 bài I Phi-líp đoạn 2",
+      ],
+      correctAnswers: [
+        "1",
+        "2",
+        "3",
+        "4",
+        "5",
+        "6",
+        "7",
+        "8",
+        "9",
+        "10",
+        "11",
+        "12",
+        "13",
+        "14",
+        "15",
+        "16",
+        "17",
+        "18",
+      ]
+  }
+};
+
+let currentTest;
+let nameModel; // Địa chỉ Kinh Thánh
+let currentQuestion = 0;
+
+document.addEventListener("DOMContentLoaded", () => {
+  const selectContainer = document.getElementById("select-container");
+  const questionContainer = document.getElementById("question-container");
+  const answerInput = document.getElementById("answer-input");
+  const submitAnswerButton = document.getElementById("submit-answer");
+  const feedback = document.getElementById("feedback");
+  const homeButton = document.getElementById("home-button");
+
+  function normalizeAnswer(answer) {
+      return answer.trim().replace(/\.+$/, '');
+  }
+
+  function displayQuestion() {
+      questionContainer.innerText = `Câu số ${currentQuestion + 1}`;
+      answerInput.value = "";
+      
+  }
+
+  function checkAnswer() {
+      const userAnswer = normalizeAnswer(answerInput.value.trim());
+      const correctAnswer = normalizeAnswer(currentTest.correctAnswers[currentQuestion]);
+
+      if (userAnswer === correctAnswer) {
+          currentQuestion++;
+          if (currentQuestion < currentTest.questions.length) {
+            displayQuestion();
+            feedback.innerText = `Câu ${currentQuestion} chính xác`;
+          } else {
+            feedback.innerText = "Chúc mừng! Bạn đã hoàn thành!";
+            submitAnswerButton.style.display = "none";
+            alert(`Chúc mừng! Bạn đã HOÀN THÀNH đoạn ${nameModel}`);
+          }
+      } else {
+          var verseNumber = currentQuestion + 1;
+          feedback.innerText = "Sai rồi! Bắt đầu lại từ đầu.";
+          alert(`Câu ${verseNumber} chưa chính xác:\nĐáp án: ${correctAnswer}\nBạn nhập: ${userAnswer}`);
+          currentQuestion = 0;
+          displayQuestion();
+      }
+  }
+
+  document.getElementById("test1").addEventListener("click", () => {
+      currentTest = tests.test1;
+      nameModel = document.getElementById("test1").innerText;
+      startTest();
+  });
+
+  document.getElementById("test2").addEventListener("click", () => {
+      currentTest = tests.test2;
+      nameModel = document.getElementById("test2").innerText;
+      startTest();
+  });
+
+  function startTest() {
+      selectContainer.style.display = "none";
+      questionContainer.style.display = "block";
+      answerInput.style.display = "block";
+      submitAnswerButton.style.display = "block";
+      homeButton.style.display = "block"
+      currentQuestion = 0;
+      displayQuestion();
+  }
+
+  homeButton.addEventListener("click", () => {
+    selectContainer.style.display = "block";
+    questionContainer.style.display = "none";
+    answerInput.style.display = "none";
+    submitAnswerButton.style.display = "none";
+    homeButton.style.display = "none";
+    feedback.innerText = "";
+});
+
+  submitAnswerButton.addEventListener("click", checkAnswer);
+  answerInput.addEventListener("keypress", (e) => {
+      if (e.key === "Enter") {
+        e.preventDefault();
+          checkAnswer();
+      }
+  });
+});
